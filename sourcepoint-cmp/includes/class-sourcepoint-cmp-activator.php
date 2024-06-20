a1eef9d2b4a16ce557f8a00122cd4a5e864e9628
git <atanur@sourcepoint.com>
2024-06-20 12:39:03 +0200

    inital commit
    version 1.0.0

---
+++ sourcepoint-cmp/includes/class-sourcepoint-cmp-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://sourcepoint.com
+ * @since      1.0.0
+ *
+ * @package    Sourcepoint_Cmp
+ * @subpackage Sourcepoint_Cmp/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Sourcepoint_Cmp
+ * @subpackage Sourcepoint_Cmp/includes
+ * @author     Atanur Demirci <atanur@sourcepoint.com>
+ */
+class Sourcepoint_Cmp_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
